230c9942a14f59cd97ff7af855eacbacfeead1cf
davipatez <davipatez2603@gmail.com>
2024-03-17 13:41:39 -0300

    v1

--- projeto.py
+++ projeto.py
@@ -1,1 +1,37 @@
 # O projeto sera desenvolvido aqui
+
+#  Passo 1 - Clicar e digitar usuario e senha
+#  Passo 2 - Passar linha por linha
+#  Passo 3 - cadastrar todas as as informações para o sistema de cadastro
+
+import pyautogui
+
+pyautogui.PAUSE = 1.5
+
+pyautogui.press('win')
+pyautogui.write('Chorme')
+pyautogui.press('enter')
+pyautogui.click(x=894, y=638)
+pyautogui.write('https://web.whatsapp.com/')
+pyautogui.click(x=241, y=268)
+
+pyautogui.write('dipo')
+pyautogui.press('enter')
+pyautogui.write('Olá, meu caro amigo, se vc recebeu essa mensagem, voce foi selecionado pra receber essa mensagem como um teste de uma bot em python que eu to fazendo. Acredite, eu nem toquei no teclado pra te mandar isso. É isso ai, meu bom, te amo e tmj safado <3', interval=0.15)
+pyautogui.press('enter')
+
+# pyautogui.click(x=241, y=268)
+# pyautogui.write('muga')
+# pyautogui.press('enter')
+# pyautogui.write('Olá,meu caro amigo, se vc recebeu essa mensagem, voce foi selecionada pra receber essa mensagem commo um teste de uma bot em python que eu to fazendo. Acredite, eu nem toquei no teclado pra te mandar isso. É isso ai, meu bom, te amo e tmj safado <3')
+# pyautogui.press('enter')
+
+# pyautogui.click(x=241, y=268)
+# pyautogui.write('Jão Vitor')
+# pyautogui.press('enter')
+# pyautogui.write('Olá,meu caro amigo, se vc recebeu essa mensagem, voce foi selecionada pra receber essa mensagem commo um teste de uma bot em python que eu to fazendo. Acredite, eu nem toquei no teclado pra te mandar isso. É isso ai, meu bom, te amo e tmj safado <3')
+# pyautogui.press('enter')
+
+
+# pyautogui.press('tab')
+# pyautogui.write('pulapula1234')
